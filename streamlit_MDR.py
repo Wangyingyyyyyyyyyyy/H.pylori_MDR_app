502be05a2a98324e095778639fe7ad1b24562314
Wangyingyyyyyyyyyyy <143474615+Wangyingyyyyyyyyyyy@users.noreply.github.com>
2024-07-09 01:17:25 +0800

    Add files via upload

--- streamlit_MDR.py
+++ streamlit_MDR.py
@@ -1,4 +1,4 @@
-# 导入需要的库
+
 import streamlit as st
 import pandas as pd
 import joblib
@@ -41,17 +41,17 @@ for i in range(3):
 with st.form("my_form"):
     col7, col8 = st.columns([5, 5])
     with col7:
-        a = st.selectbox("rlpA Thr216Lys", ("presence", "absence"))
-        b = st.selectbox("group_1364", ("presence", "absence"))
-        c = st.selectbox("glmU Glu162Thr", ("presence", "absence"))
-        d = st.selectbox("HP_0731 Asn511Asp", ("presence", "absence"))
-        e = st.selectbox("smc", ("presence", "absence"))
+        a = st.selectbox("group_354", ("presence", "absence"))
+        b = st.selectbox("hyuA K524E", ("presence", "absence"))
+        c = st.selectbox("group_667", ("presence", "absence"))
+        d = st.selectbox("group_1303", ("presence", "absence"))
+        e = st.selectbox("pabC E150A", ("presence", "absence"))
     with col8:
-        f = st.selectbox("gspA", ("presence", "absence"))
-        g = st.selectbox("group_333", ("presence", "absence"))
-        h = st.selectbox("polA Val112Thr", ("presence", "absence"))
-        i = st.selectbox("omp13 SerLeu10PhePhe", ("presence", "absence"))
-        j = st.selectbox("HP_0922 Ser2141Ala", ("presence", "absence"))
+        f = st.selectbox("omp18 K168R", ("presence", "absence"))
+        g = st.selectbox("HP0415 T247M", ("presence", "absence"))
+        h = st.selectbox("group_1307", ("presence", "absence"))
+        i = st.selectbox("group_541", ("presence", "absence"))
+        j = st.selectbox("omp13 G211A", ("presence", "absence"))
     col4, col5, col6 = st.columns([2, 2, 6])
     with col4:
         submitted = st.form_submit_button("Predict")
@@ -59,31 +59,31 @@ with st.form("my_form"):
         reset = st.form_submit_button("Reset")
 
 
-    # 如果按下按钮
-    if submitted:  # 显示按钮
-        # 将输入存储DataFrame
+
+    if submitted:
+
         X = pd.DataFrame([[a, b, c, d, e, f, g, h, i, j]],
-                         columns=['rlpA Thr216Lys', 'group_1364', 'glmU Glu162Thr', 'HP_0731 Asn511Asp', 'smc', 'gspA', 'group_333', 'polA Val112Thr', 'omp13 SerLeu10PhePhe', 'HP_0922 Ser2141Ala'])
+                         columns=['group_354', 'hyuA K524E', 'group_667', 'group_1303', 'pabC E150A', 'omp18 K168R', 'HP0415 T247M', 'group_1307', 'group_541', 'omp13 G211A'])
     
         X = X.replace(["presence", "absence"], [1, 0])
     
     
-        # 进行预测
+
         prediction = model.predict(X)[0]
         Predict_proba = model.predict_proba(X)[:, 1][0]
-        # 输出预测结果
+
         if prediction == 0:
             st.subheader(f"The predicted result of Hp MDR:  Sensitive")
         else:
             st.subheader(f"The predicted result of Hp MDR:  Resistant")
-        # 输出概率
+
         st.subheader(f"The probability of Hp MDR:  {'%.2f' % float(Predict_proba * 100) + '%'}")
 
         with st.spinner('force plot generation, please wait...'):
             explainer = shap.TreeExplainer(model)
             shap_values = explainer.shap_values(X)
-            shap.force_plot(explainer.expected_value, shap_values[0], X.iloc[0].values, feature_names=['rlpA Thr216Lys', 'group_1364', 'glmU Glu162Thr', 'HP_0731 Asn511Asp', 
-                                                                                                       'smc', 'gspA', 'group_333', 'polA Val112Thr', 'omp13 SerLeu10PhePhe', 'HP_0922 Ser2141Ala'], matplotlib=True, show=False, figsize=(20, 5))
+            shap.force_plot(explainer.expected_value, shap_values[0], X.iloc[0].values, feature_names=['group_354', 'hyuA K524E', 'group_667', 'group_1303',
+                                                                                                       'pabC E150A', 'omp18 K168R', 'HP0415 T247M', 'group_1307', 'group_541', 'omp13 G211A'], matplotlib=True, show=False, figsize=(20, 5))
             plt.xticks(fontproperties='Arial', size=16)
             plt.yticks(fontproperties='Arial', size=16)
             plt.tight_layout()
